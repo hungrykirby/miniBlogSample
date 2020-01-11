1c2b5d3a34d4ec13e6c206232e51a95c066d3c58
hungrykirby <katsuki8hanawa6@gmail.com>
2020-01-11 23:23:57 +0900

    ユーザ詳細（作成）に関して機能追加

--- app/controllers/admins/display_user_contents_controller.rb
+++ app/controllers/admins/display_user_contents_controller.rb
@@ -15,8 +15,26 @@ class Admins::DisplayUserContentsController < ApplicationController
 
     respond_to do |format|
       if @display_user_content.save
-        # format.html { redirect_to @dis}
+        format.html { redirect_to @display_user_content, notice: '表示ユーザ詳細を更新しました' }
+        format.json { render :show, status: :created, location: @display_user_content }
+      else
+        format.html { render :new }
+        format.json { render json: @display_user_content.errors, status: :unprocessable_entity }
       end
     end
   end
+
+  private
+
+  def set_content
+    @display_user_content = DisplayUserContent.find(params[:id])
+  end
+
+  def content_params
+    params.require(:display_user_content).permit(
+      :title,
+      :body
+    )
+  end
+
 end
